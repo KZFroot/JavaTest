e67a98681db815c21ddbf699aa6bf9b714e62177
“KZFroot” <15172054680@163.com>
2021-05-02 17:31:54 +0800

    0502

--- src/com/collection1/LinkedHashMapTest.java
+++ src/com/collection1/LinkedHashMapTest.java
@@ -1,5 +1,13 @@
 package com.collection1;
 
+import java.util.LinkedHashMap;
 public class LinkedHashMapTest {
-    
+    public static void main(String[] args) {
+        LinkedHashMap scores=new LinkedHashMap();
+        scores.put("语文",80);
+        scores.put("英语",82);
+        scores.put("数学",76);
+        //调用forEach()方法遍历scores里面的所有key-value对
+       scores.forEach((key, value)-> System.out.println(key+"-->"+value));
+    }
 }
